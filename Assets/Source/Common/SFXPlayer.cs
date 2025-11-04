859418cde03241696f56371f71b9505d23b652fb
Quinton <quinncg.dev@gmail.com>
2025-11-04 12:49:01 -0500

    Player footstep SFX

--- Assets/Source/Common/SFXPlayer.cs
+++ Assets/Source/Common/SFXPlayer.cs
@@ -1,11 +1,31 @@
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Quinn
 {
 	public class SFXPlayer : MonoBehaviour
 	{
-		public void Play(string eventPath)
+		[SerializeField, Unit(Units.Second)]
+		private float CooldownPerUniqueEvent;
+
+		private readonly Dictionary<string, float> _pathToCooldownEndTime = new();
+
+		public void SFX(string eventPath)
 		{
+			if (CooldownPerUniqueEvent > 0f && _pathToCooldownEndTime.ContainsKey(eventPath))
+			{
+				if (_pathToCooldownEndTime[eventPath] > Time.time)
+				{
+					return;
+				}
+				else
+				{
+					_pathToCooldownEndTime.Remove(eventPath);
+				}
+			}
+
+			_pathToCooldownEndTime.Add(eventPath, Time.time + CooldownPerUniqueEvent);
 			Audio.Play(eventPath, transform.position);
 		}
 	}
