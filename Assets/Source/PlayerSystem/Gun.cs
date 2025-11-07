538c8882b2f7c57dd6d0247a9481b849da0e1705
Quinton <quinncg.dev@gmail.com>
2025-11-06 20:56:48 -0500

    Work on gun system

--- Assets/Source/PlayerSystem/Gun.cs
+++ Assets/Source/PlayerSystem/Gun.cs
@@ -1,3 +1,4 @@
+using FMODUnity;
 using Quinn.MissileSystem;
 using UnityEngine;
 
@@ -8,9 +9,15 @@ namespace Quinn.PlayerSystem
 	{
 		public Sprite Sprite;
 		public Sprite AmmoUI;
+
+		public Vector2 MuzzleOffset;
 		public MissileSpawnData Missile;
+
 		public float FireRate = 0.5f;
 		public int MagazineSize = 20;
 		public float ReloadTime = 2f;
+
+		public EventReference FireSound, DryFireSound;
+		public EventReference ReloadStartSound, ReloadFinishSound;
 	}
 }
--- Assets/Source/PlayerSystem/GunManager.cs
+++ Assets/Source/PlayerSystem/GunManager.cs
@@ -1,17 +1,70 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Quinn.PlayerSystem
 {
 	public class GunManager : MonoBehaviour
 	{
+		[SerializeField, Required]
+		private SpriteRenderer GunSprite;
+		[SerializeField, Required]
+		private Transform MuzzleTransform;
+
+		public Gun Equipped { get; private set; }
+		public bool IsFiring { get; private set; }
+		public int Magazine { get; private set; }
+
+		public event System.Action<Gun> OnEquipped;
+		public event System.Action OnUnequipped;
+
+		public void Equip(Gun gun)
+		{
+			Equipped = gun;
+			GunSprite.sprite = gun.Sprite;
+			ReplenishMagazine();
+			OnEquipped?.Invoke(gun);
+		}
+
+		public void Unequip()
+		{
+			GunSprite.sprite = null;
+			Equipped = null;
+			OnUnequipped?.Invoke();
+		}
+
+		public void ReplenishMagazine()
+		{
+			if (Equipped != null)
+			{
+				Magazine = Equipped.MagazineSize;
+				Audio.Play(Equipped.ReloadFinishSound, transform);
+			}
+		}
+
 		public void StartFiring()
 		{
-
+			if (!IsFiring)
+			{
+				IsFiring = true;
+			}
 		}
 
 		public void StopFiring()
 		{
+			if (IsFiring)
+			{
+				IsFiring = false;
+			}
+		}
 
+		private Vector2 GetMuzzlePoint()
+		{
+			
+		}
+
+		private void Recoil(float offset, float recoveryTime)
+		{
+			GunOrbiter.Instance.Recoil(offset, recoveryTime);
 		}
 	}
 }
