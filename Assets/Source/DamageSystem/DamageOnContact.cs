453347730ab08b9f2f316a4823b1646fbdc9e042
Quinton <quinncg.dev@gmail.com>
2025-11-14 17:29:09 -0500

    Damage on contact

---
+++ Assets/Source/DamageSystem/DamageOnContact.cs
@@ -0,0 +1,67 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quinn.DamageSystem
+{
+	[RequireComponent(typeof(Collider2D))]
+	public class DamageOnContact : MonoBehaviour
+	{
+		[SerializeField]
+		private float Damage = 1f;
+		[SerializeField]
+		private float Knockback = 8f;
+		[SerializeField]
+		private StatusEffectInstance[] Statuses;
+
+		[Space]
+
+		[SerializeField, ShowIf("@GetComponent<Health>() == null")]
+		private Team Team;
+
+		private Collider2D _collider;
+		private Team _team;
+
+		private void Awake()
+		{
+			_collider = GetComponent<Collider2D>();
+
+			if (TryGetComponent(out Health health))
+			{
+				_team = health.Team;
+			}
+			else
+			{
+				_team = Team;
+			}
+		}
+
+		private void FixedUpdate()
+		{
+			var results = new List<Collider2D>();
+			Physics2D.OverlapCollider(_collider, new ContactFilter2D()
+			{
+				layerMask = ~0,
+				useLayerMask = true
+			}, results);
+
+			foreach (var collider in results)
+			{
+				if (collider == _collider)
+					continue;
+
+				if (collider.TryGetComponent(out Health health))
+				{
+					health.TryTakeDamage(new DamageInfo()
+					{
+						Damage = Damage,
+						Team = _team,
+						StatusEffects = Statuses,
+						Direction = transform.position.DirectionTo(health.transform.position),
+						Knockback = Knockback
+					});
+				}
+			}
+		}
+	}
+}
