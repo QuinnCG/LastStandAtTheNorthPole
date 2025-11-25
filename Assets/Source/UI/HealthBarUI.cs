5212f94e4c0e09af24d451fdd746f7c578c18346
Quinton <quinncg.dev@gmail.com>
2025-11-25 12:40:58 -0500

    Enemy health bars

---
+++ Assets/Source/UI/HealthBarUI.cs
@@ -0,0 +1,67 @@
+﻿using DG.Tweening;
+using Quinn.DamageSystem;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Quinn.UI
+{
+	[RequireComponent(typeof(Health))]
+	public class HealthBarUI : MonoBehaviour
+	{
+		[SerializeField, Required]
+		private Slider HPBar;
+		[SerializeField, Required]
+		private CanvasGroup CanvasGroup;
+
+		[SerializeField]
+		private float FadeInTime = 0.1f, FadeOutTime = 0.5f;
+		[SerializeField]
+		private float PunchScale = 1.1f, PunchTime = 0.1f;
+
+		private Health _health;
+
+		private void Awake()
+		{
+			CanvasGroup.alpha = 0f;
+			_health = GetComponent<Health>();
+
+			_health.OnDamaged += _ =>
+			{
+				HPBar.transform.DOScale(Vector3.one * PunchScale, PunchTime)
+					.SetLoops(2, LoopType.Yoyo);
+
+				if (_health.Normalized <= 0f)
+				{
+					CanvasGroup.DOFade(0f, FadeOutTime);
+				}
+				else
+				{
+					CanvasGroup.DOFade(1f, FadeInTime);
+				}
+			};
+
+			_health.OnHeal += _ =>
+			{
+				if (_health.Normalized >= 1f)
+				{
+					CanvasGroup.DOFade(0f, FadeOutTime);
+				}
+			};
+		}
+
+		private void OnDestroy()
+		{
+			if (HPBar != null)
+			{
+				HPBar.transform.DOKill();
+				CanvasGroup.DOKill();
+			}
+		}
+
+		private void LateUpdate()
+		{
+			HPBar.value = _health.Normalized;
+		}
+	}
+}
