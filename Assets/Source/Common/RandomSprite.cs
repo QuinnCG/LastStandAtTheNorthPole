b6728dfb08c3710ada7df7885f2a602a8aa86d7b
Quinton <quinncg.dev@gmail.com>
2025-11-28 18:03:42 -0500

    Grass foliage

--- Assets/Source/Common/RandomSprite.cs
+++ Assets/Source/Common/RandomSprite.cs
@@ -6,6 +6,8 @@ namespace Quinn
 	public class RandomSprite : MonoBehaviour
 	{
 		[SerializeField]
+		private bool RandomFlipX = true;
+		[SerializeField]
 		private Sprite[] Sprites = System.Array.Empty<Sprite>();
 
 		private void Awake()
@@ -13,7 +15,14 @@ namespace Quinn
 			if (gameObject.TryGetComponent(out SpriteRenderer renderer))
 			{
 				renderer.sprite = Sprites.GetRandom();
+				
+				if (RandomFlipX)
+				{
+					renderer.flipX = Random.value < 0.5f;
+				}
 			}
+
+			this.Destroy();
 		}
 	}
 }
---
+++ Assets/Source/FoliageSpawner.cs
@@ -0,0 +1,48 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+
+namespace Quinn
+{
+	[RequireComponent(typeof(BoxCollider2D))]
+	public class FoliageSpawner : MonoBehaviour
+	{
+		[SerializeField, Required]
+		private GameObject Prefab;
+		[SerializeField, Range(0f, 1f)]
+		private float SpawnChance = 0.5f;
+		[SerializeField]
+		private float NudgeOffset = 0.2f;
+		[SerializeField]
+		private int MaxBatchIterationCount = 100;
+
+		private IEnumerator Start()
+		{
+			var bounds = GetComponent<BoxCollider2D>().bounds;
+
+			int iteration = 0;
+
+			for (int x = Mathf.RoundToInt(bounds.Left().x); x <= Mathf.RoundToInt(bounds.Right().x); x++)
+			{
+				for (int y = Mathf.RoundToInt(bounds.Bottom().y); y <= Mathf.RoundToInt(bounds.Top().y); y++)
+				{
+					if (Random.value < SpawnChance)
+					{
+						var pos = new Vector2(x, y);
+						pos += Random.insideUnitCircle * NudgeOffset;
+						Prefab.Clone(pos, transform);
+					}
+
+					iteration++;
+
+					if (iteration > MaxBatchIterationCount)
+					{
+						// Wait untill next frame.
+						iteration = 0;
+						yield return null;
+					}
+				}
+			}
+		}
+	}
+}
